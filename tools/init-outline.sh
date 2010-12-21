b2fcd0a1d3168e2a6210747d20d2e707c800f972
B. van Berkum <dev@dotmpe.com>
2010-12-21 21:46:02 +0100

    Added idea about dump file for node structure. Document format itself needs work too (first, I think).

---
+++ tools/init-outline.sh
@@ -0,0 +1,12 @@
+Docuverse I
+  Permascroll
+    | System Directory
+      | Home
+      | Link Document  
+      | Permascroll introdution
+  Node 1.2
+    Node 1.2.1
+      | Directory 1.2.1/1
+    Node 1.2.2
+  Node 1.3
+Docuverse II  
